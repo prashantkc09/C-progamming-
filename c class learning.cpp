68d70c4bf1aa860e2ac4af5f99df0eafb11f0133
Prashant Chaudhary <114799837+prashantkc09@users.noreply.github.com>
2023-04-17 22:42:56 +0530

    Add files via upload
    c learning

---
+++ c class learning.cpp
@@ -0,0 +1,133 @@
+//#include<stdio.h>
+//int main(){
+//	int x=3,y,z;
+//	printf("\nInput your data","x,y,x");
+//	scanf("\n%d,%d,%d");
+//	y=x++;
+//	z=++x;
+//	printf("\n%d,%d,%d",x,y,z);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main(){
+//	int a=1,b=1,c;
+//	printf("enter the value",a,b");
+//	scanf("%d,%d,");
+//	c=a++ +b;
+//	printf("%d,%d,%d",a,b,c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main(){
+//	int x=4,y,z;
+//	y=--x;
+//	z=x--;
+//	printf("%d,%d,%d",x,y,z);
+//	return 0;
+//}
+
+// C program to implement
+// scanf
+//#include <stdio.h>
+
+// Driver code
+//int main()
+//{
+//	int a, b;
+//
+//	printf("Enter first number: ");
+//	scanf("%d", &a);
+//
+//	printf("Enter second number: ");
+//	scanf("%d", &b);
+//
+//	printf("A : %d \t B : %d" ,
+//			a , b);
+//
+//	return 0;
+//}
+
+
+//write a peogram whether the number entered by user is smallest or greatest out of two variable  
+
+
+
+//int main ()
+//{
+//	int a,b;
+//	
+//	printf("Enter the value of a: ");
+//	scanf("%d", &a);
+//		
+//	printf("Enter the value of b : ");	
+//	scanf("%d", &b);
+//	
+//	printf("a : %d \n b : %d" , a,b);
+//	
+//	if (a>b)
+//	{
+//		printf("The value of a  is greater");
+//	}
+//	else
+//	{
+//		printf("The value of a is smaller");
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int marks,attendance;
+//	
+//	printf("Enter your marks: ");
+//	scanf("%d", &marks);
+//	
+//	printf("Enter your attendance: ");
+//	scanf("%d", &attendance);
+//	
+//	printf("marks : %d \nattendance : %d ",marks,attendance);
+//	
+//	if(marks>60 && attendance>=75)
+//	{
+//		printf("good");
+//	}
+//	else
+//	{
+//		printf("attend the classes & get good marks");
+//	}
+//	return 0;
+//
+//}
+
+#include <stdio.h>
+int main()
+{
+	int a=2,b=4;
+	printf("a=%d,b=%d\n",a,b);
+	printf("a&b=%d\n",a&b);
+	printf("a|b=%d\n",a|b);
+	printf("a^b=%d\n",a^b);
+	printf("~a=%d\n",a=~a);
+	printf("b<<1=%d\n",b<<1);
+	printf("b>>1=%d\n",b>>1);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
